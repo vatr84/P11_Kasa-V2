66d23572cd1d58dab2da3dd28159876ce049d7f3
vatr84 <vatr@hotmail.fr>
2025-02-03 02:43:21 +0100

    Mobile_404

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,8 +5,10 @@ import Logo from '../Logo'
 export default function Footer() {
     return (
         <footer className="footer">
-            <Logo color="currentColor" />
-            <p className="footer__credit">© 2020 Kasa - All rights reserved</p>
+            <Logo className="logo" color="currentColor" />
+            <p className="footer__credit">
+                © 2020 Kasa. All<br className="footer__credit-break" /> rights reserved
+            </p>
         </footer>
     )
 }
--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -7,9 +7,9 @@ export default function NotFound() {
     return (
         <main className="notFound container">
             <img src={notFound404} alt="" />
-            <h1>Oups! La page que vous demandez n&apos;existe pas.</h1>
+            <h1 className="notFound__title">Oups! La page que<br className="notFound__title-break" /> vous demandez n&apos;existe pas.</h1>
             <p>
-                <Link to="/">Retrouner sur la page d&apos;accueil</Link>
+                <Link to="/" className="notFound__link">Retrouner sur la page d&apos;accueil</Link>
             </p>
         </main>
     )
